aec12ce278074c72f1269556895202a36c8a6025
Dilay Yigit <72208914+DilayYigit@users.noreply.github.com>
2021-08-07 18:37:00 +0300

    question 206 is added.

---
+++ Reverse_LinkedList.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) { 
+        if (head == NULL || head->next == NULL) {
+            return head;
+        }
+        ListNode* newHead = reverseList(head->next);
+        head->next->next = head;
+        head->next = NULL;
+        
+       return newHead;
+    }
+};
