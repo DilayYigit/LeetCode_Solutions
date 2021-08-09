fd4e1b06d91f9f5702661e0cd631f1a0128927df
Dilay Yigit <72208914+DilayYigit@users.noreply.github.com>
2021-08-09 16:21:55 +0300

    Update question_496.cpp

--- question_496.cpp
+++ question_496.cpp
@@ -3,40 +3,38 @@ public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         stack<int> stack;
 
-    for (int i = 0; i < nums1.size(); i++) {
-
-        for (int j = nums2.size() - 1; j >= 0; j--) {
-            stack.push(nums2[j]);
-        }
-
-        while (!stack.empty()) {
-            if (nums1[i] == stack.top()) {
-                int temp = stack.top();
-                stack.pop();
-                int greater = temp;
-                while (!stack.empty()) {
-                    if (stack.top() > temp) {
-                        greater = stack.top();
-                        break;
+        for (int i = 0; i < nums1.size(); i++) {
+            for (int j = nums2.size() - 1; j >= 0; j--) {
+                stack.push(nums2[j]);
+            }
+            while (!stack.empty()) {
+                if (nums1[i] == stack.top()) {
+                    int temp = stack.top();
+                    stack.pop();
+                    int greater = temp;
+                    while (!stack.empty()) {
+                        if (stack.top() > temp) {
+                            greater = stack.top();
+                            break;
+                        }
+                        else
+                            stack.pop();
+                    }
+                    if (greater != temp) {
+                        nums1[i] = greater;
                     }
                     else
-                        stack.pop();
-                }
-                if (greater != temp) {
-                    nums1[i] = greater;
+                        nums1[i] = -1;
+                    break;
                 }
                 else
-                    nums1[i] = -1;
-                break;
+                    stack.pop();
             }
-            else
-                stack.pop();
-        }
 
-        while (!stack.empty()) {
-            stack.pop();
+            while (!stack.empty()) {
+                stack.pop();
+            }
         }
-    }
-    return nums1;
+        return nums1;
     }
 };
