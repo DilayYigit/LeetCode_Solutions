f8c0de65b7e53b10942956c52a4f4e226f66952d
Dilay Yigit <72208914+DilayYigit@users.noreply.github.com>
2021-11-18 13:56:46 +0300

    Create question_111.cpp

---
+++ question_111.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if (root == NULL) {
+            return 0;
+        }
+        if (root->left == NULL && root->right != NULL) {
+            return 1 + minDepth(root->right);
+        }
+        if (root->left != NULL && root->right == NULL) {
+            return 1 + minDepth(root->left);
+        }
+        return 1 + min(minDepth(root->left), minDepth(root->right)); 
+    } 
+};
