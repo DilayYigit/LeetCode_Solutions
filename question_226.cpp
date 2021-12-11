4f9e50d6b90e0389c34ee29e60f4652cc0097ed3
Dilay Yigit <72208914+DilayYigit@users.noreply.github.com>
2021-12-11 17:18:55 +0300

    Create question_226.cpp

---
+++ question_226.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (root != NULL) {
+            TreeNode* temp = root->left;
+            root->left = root->right;
+            root->right = temp;
+            invertTree(root->left);
+            invertTree(root->right);
+        }
+        return root;
+    }
+
+};
