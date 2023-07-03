0f9696de59f513f8ef707e761eafa199dc9bd320
Dilay Yigit <72208914+DilayYigit@users.noreply.github.com>
2023-07-03 14:22:15 +0300

    Create question_1071.cpp

---
+++ question_1071.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        if (str1 + str2 != str2 + str1) return "";
+        else if (str1 == str2) return str1;
+        else if (str1.length() > str2.length()) return gcdOfStrings(str1.substr(str2.length()), str2);
+        else if (str1.length() < str2.length()) return gcdOfStrings(str2.substr(str1.length()), str1);
+        return "";
+    }
+};
